cc8419cd9db9c4ce59d9c005a75e1b723bc2de06
Huy Tran <huycamhuy@gmail.com>
2024-03-09 15:05:10 +0700

    Edit student.h

--- SOURCE CODE/STUDENT/student.h
+++ SOURCE CODE/STUDENT/student.h
@@ -2,4 +2,12 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+void pressEnterToContinue();
+void displayLoginConsole();
+void login();
+void displayStudentMenu();
+void displayStudentProfile();
+void displayCourses();
+void changePassword();
+
 #endif // STUDENT_H
