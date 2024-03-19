9d2b1820ba7feb806087b98a31b16c1be254286d
Buh Duy <148170993+BuhDuy256@users.noreply.github.com>
2024-03-19 21:57:48 +0700

    Update struct.h Team meeting:19/3/2024

--- SOURCE/struct.h
+++ SOURCE/struct.h
@@ -26,57 +26,67 @@ struct Node {
 	Node(T dataValue, Node<T>* nextNode) : data(dataValue), next(nextNode) {}
 };
 
-struct SchoolYear {
-	string year;	
-	Semester semesters[3];
-};
-
 struct Class { // Class is not linked to linked list of School Year
 	string className;
 	string schoolYear; 
-	Node<int>* listStuID;   
+	Student* students = nullptr;   
+};
+
+struct Staff {
+	string ID;
+	string name;
+	string gender;
+	string socialID;
+	string password = "123445678";
 };
 
 struct Student { // Dynamic array of struct
-	int studentID;
-    string studentName;
+	string ID;
+    string name;
     string gender;
     string birthday;
     string socialID;
     string mainClass; // *
-    string password; // *
-	Node<string>* stuCourses = nullptr; // Storing List of all courses ID that student studied 
-};
-
-struct Course { // Singly linked list (Add course in head of list, not tail)
-	string courseID; // have character 
-    string courseName;
-    string className;
-    string teacherName;
-    string courseSize = "50";
-    string dayOfWeek;
-    string session;
-	StudentScore score[50];
+    string password = "12345678"; // *
+	Course** addressCourses = nullptr; // Store Address Of Course Node
+	void viewProfile();
+	void viewCourses();
+	void viewScores();
 };
 
 struct StudentScore { // An array 
 	string studentID;
 	string studentName;
-	double midTerm;
-	double final;
-	double other;
-	double total;
-	double GPA;
+	double midTerm = -1;
+	double final = -1;
+	double other = -1;
+	double total = -1;
+	double GPA = -1;
 	StudentScore () : midTerm(0), final(0), other(0), total(0), GPA(0) {}
 };
 
+struct Course { // Singly linked list (Add course in head of list, not tail)
+	string ID; // have character 
+    string Name;
+    string className;
+    string teacherName;
+    int courseSize = 50;
+    string dayOfWeek;
+    string session;
+	StudentScore* score = nullptr;
+};
+
 struct Semester {
-	unsigned short semesterNumber; // whyneedthis
+	unsigned short number = 0; 
 	string startDate;
 	string endDate;
 	Node<Course>* Courses = nullptr;
 };
 
+struct SchoolYear {
+	string year;	
+	Semester semesters[3];
+};
 
 
 
