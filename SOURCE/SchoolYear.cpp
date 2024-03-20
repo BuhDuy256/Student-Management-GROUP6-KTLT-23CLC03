ed2956baeab7356a195a0e66be4f066d8c71db8b
Your Name <Vanvu151105@gmail.com>
2024-03-20 17:27:15 +0700

    add

--- SOURCE/SchoolYear.cpp
+++ SOURCE/SchoolYear.cpp
@@ -1,1 +1,10 @@
 #include"SchoolYear.h"
+void createASchoolYear(Node<SchoolYear>* &CurYear){
+ string newyear;
+ cout << "ENTER CURRENT YEAR: ";
+ cin >> newyear;
+ SchoolYear newy;
+ newy.year = newyear;
+ Node<SchoolYear>* NewYear = new Node<SchoolYear>(newy,CurYear);
+ CurYear = NewYear;
+}
