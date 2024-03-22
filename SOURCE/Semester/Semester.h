4cb4da34cc48cb2126c13669041fe2bef761d4e9
Huy Tran <huycamhuy@gmail.com>
2024-03-23 00:00:49 +0700

    Implement add a course to a semester

---
+++ SOURCE/Semester/Semester.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Node.h"
+#include "Course.h"
+#include <iostream>
+#include <iomanip>
+
+struct Semester {
+	unsigned short number = 0; 
+	std::string startDate;
+	std::string endDate;
+	Node<Course>* coursesList = nullptr;
+
+	void addCourse(Course course);
+	void deleteCourse(Course course);
+	void viewCourses();
+};
