13cd437ee60af619b0d5fb18c10f039b6b6d8eee
Huy Tran <huycamhuy@gmail.com>
2024-03-26 23:41:51 +0700

    Restore to original

--- SOURCE/Course.cpp
+++ SOURCE/Course.cpp
@@ -1,2 +1,146 @@
-#include"Course.h"
+#include "Course.h"
+#include "System.h"
 
+void Course::updateInformation()
+{
+     // Display a menu of options for information update
+    std::cout << "What information do you want to update?" << std::endl;
+    std::cout << "1. Class Name" << std::endl;
+    std::cout << "2. Teacher Name" << std::endl;
+    std::cout << "3. Day of Week" << std::endl;
+    std::cout << "4. Session" << std::endl;
+
+    int choice;
+    while (true)
+        if (getChoice(4, "Enter the number corresponding to your choice: ", choice)) break;
+
+    // Prompt for the new value based on the user's choice
+    std::string newValue;
+    switch (choice) {
+        case 1:
+            std::cout << "Enter the new Class Name: ";
+            std::getline(std::cin, newValue);
+            className = newValue;
+            break;
+        case 2:
+            std::cout << "Enter the new Teacher Name: ";
+            std::getline(std::cin, newValue);
+            teacherName = newValue;
+            break;
+        case 3:
+        {
+            std::string daysOfWeek[6] = {"MON", "TUE", "WED", "THU", "FRI", "SAT"};
+            std::cout << "Select the new day of week:\n";
+            for (int i = 0; i < 6; ++i) std::cout << i + 1 << ". " << daysOfWeek[i] << std::endl;
+            int choice;
+            while (true)
+                if (getChoice(6, "Enter the number of the new day of week: ", choice)) break;
+            dayOfWeek = dayOfWeek[choice - 1];
+            break;
+        }
+        case 4:
+        {
+            std::string sessions[4] = {"S1 (07:30)", "S2 (09:30)", "S3 (13:30)", "S4 (15:30)"};
+            std::cout << "Select the new session:\n";
+            for (int i = 0; i < 4; ++i) std::cout << i + 1 << ". " << sessions[i] << std::endl;
+            int choice;
+            while (true)
+                if (getChoice(4, "Enter the number of the new session: ", choice)) break;
+            session = sessions[choice - 1];
+            break;
+        }
+    }
+    std::cout << "Information updated successfully." << std::endl;
+}
+
+void Course::viewStudentsList()
+{
+    if (scoreList != nullptr) 
+    {
+        std::cout << std::left << std::setw(10) << "Index" << std::setw(15) << "Student ID" << std::setw(25) << "Student Name" << std::endl;
+        std::cout << std::setfill('-') << std::setw(50) << "-" << std::setfill(' ') << std::endl;
+
+        for (int i = 0; i < courseSize; ++i) 
+        {
+            std::cout << std::left << std::setw(10) << (i + 1) << std::setw(15) << scoreList[i].studentID << std::setw(25) << scoreList[i].studentName << std::endl;
+            std::cout << std::setfill('-') << std::setw(50) << "-" << std::setfill(' ') << std::endl;
+        }
+    } 
+    else std::cout << "No students in the course." << std::endl;
+}
+
+void Course::addStudent(std::string studentID, std::string studentName)
+{
+    StudentScore* newScoreList = new StudentScore [courseSize + 1]; 
+    for (int i = 0; i < courseSize; i++)
+        newScoreList[i] = scoreList[i];
+    newScoreList[courseSize].studentID = studentID;
+    newScoreList[courseSize].studentName = studentName;
+    
+    delete [] scoreList;
+    scoreList = newScoreList;
+    courseSize++;
+}
+
+bool Course::deleteStudent(int index)
+{
+    if (index < 0 || index > courseSize) return false;
+    for (int i = index - 1; i < courseSize - 1; i++) scoreList[i] = scoreList[i + 1];
+    courseSize--;
+    return true;
+}
+
+void Course::viewScoreboard()
+{
+    if (scoreList != nullptr) 
+    {
+        std::cout << std::left 
+        << std::setw(10) << "Index" << std::setw(15) << "Student ID" << std::setw(25) << "Student Name" << std::setw(10) << "Midterm" 
+        << std::setw(10) << "Final" << std::setw(10) << "Other" << std::setw(10) << "Total" << std::setw(10) << "GPA" << std::endl;
+        std::cout << std::setfill('-') << std::setw(100) << "-" << std::setfill(' ') << std::endl;
+
+        for (int i = 0; i < courseSize; ++i) 
+        {
+            std::cout << std::left 
+                << std::setw(10) << (i + 1) << std::setw(15) << scoreList[i].studentID << std::setw(25) << scoreList[i].studentName
+                << std::setw(10) << scoreList[i].midTerm << std::setw(10) << scoreList[i].final
+                << std::setw(10) << scoreList[i].other << std::setw(10) << scoreList[i].total
+                << std::setw(10) << scoreList[i].GPA << std::endl;
+            std::cout << std::setfill('-') << std::setw(100) << "-" << std::setfill(' ') << std::endl;
+        }
+    } 
+    else std::cout << "No students in the course." << std::endl;
+}
+
+void Course::updateStudentResult()
+{
+    // Display the the list of students
+    std::cout << "List of Students:" << std::endl;
+    viewStudentsList();
+
+    // Prompt for student index
+    int studentIndex;
+    while (true)
+        if (getChoice(courseSize, "Enter the index of the student you want to update: ", studentIndex)) break;
+
+    // Display a menu of score types
+    std::string scoreTypes[3] = {"Midterm", "Final", "Other"};
+    std::cout << "Select the score type to update:" << std::endl;
+    for (int i = 0; i < 3; ++i) std::cout << i + 1 << ". " << scoreTypes[i] << std::endl;
+   
+    // Prompt for the user's choice
+    int choice;
+    while (true)
+        if (getChoice(3, "Enter the number corresponding to your choice: ", choice)) break;
+
+    // Prompt for the new score
+    double newScore;
+    std::cout << "Enter the new score: ";
+    std::cin >> newScore;
+
+    // Update the selected score
+    if (scoreTypes[choice - 1] == "Midterm") scoreList[studentIndex - 1].midTerm = newScore;
+    else if (scoreTypes[choice - 1] == "Final") scoreList[studentIndex - 1].final = newScore;
+    else if (scoreTypes[choice - 1] == "Other") scoreList[studentIndex - 1].other = newScore;
+    std::cout << "Student's result updated successfully." << std::endl;
+}
--- SOURCE/Course.h
+++ SOURCE/Course.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include<iostream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+struct Student;
 
 struct StudentScore {  
 	std::string studentID;
@@ -9,6 +12,7 @@ struct StudentScore {
 	double final = -1;
 	double other = -1;
 	double total = -1;
+	double GPA = -1;
 };
 
 struct Course {
@@ -19,5 +23,15 @@ struct Course {
     int courseSize = 0;
 	std::string dayOfWeek;
 	std::string session;
-	StudentScore* score = nullptr; 
+	StudentScore* scoreList = nullptr;
+
+	void updateInformation();
+	
+	void viewStudentsList();
+	void addStudent(std::string studentID, std::string studentName);
+	bool deleteStudent(int index);
+
+	void viewScoreboard();
+	void updateStudentResult();
 };
+
