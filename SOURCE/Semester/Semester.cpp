b0edefb95ad010b6dc0e227e2a15bf514df6e2d3
Huy Tran <huycamhuy@gmail.com>
2024-03-26 23:28:54 +0700

    Update viewCoursesList
    Change some format while printing

--- SOURCE/Semester/Semester.cpp
+++ SOURCE/Semester/Semester.cpp
@@ -2,15 +2,18 @@
 
 void Semester::viewCoursesList()
 {
-     std::cout << std::setw(5) << "Index" << std::setw(10) << "ID" << std::setw(30) << "Name" << std::setw(20) << "Day" << std::setw(15) 
-        << "Session" << std::setw(20) << "Class Name" << std::setw(20) << "Teacher Name" << std::endl;
-    std::cout << std::setfill('-') << std::setw(120) << "-" << std::setfill(' ') << std::endl;
+     std::cout << std::left 
+        << std::setw(10) << "Index" << std::setw(10) << "ID" << std::setw(30) << "Name" << std::setw(20) << "Day" 
+        << std::setw(15) << "Session" << std::setw(20) << "Class Name" << std::setw(20) << "Teacher Name" << std::endl;
+    std::cout << std::setfill('-') << std::setw(125) << "-" << std::setfill(' ') << std::endl;
 
     Node<Course>* course = coursesList;
     int index = 1;
     while (course != nullptr) {
-        std::cout << std::setw(5) << index << std::setw(10) << course->data.ID << std::setw(30) << course->data.name << std::setw(20) << course->data.dayOfWeek 
+        std::cout << std::left 
+            << std::setw(10) << index << std::setw(10) << course->data.ID << std::setw(30) << course->data.name << std::setw(20) << course->data.dayOfWeek 
             << std::setw(15) << course->data.session << std::setw(20) << course->data.className << std::setw(20) << course->data.teacherName << "\n"; 
+        std::cout << std::setfill('-') << std::setw(125) << "-" << std::setfill(' ') << std::endl;
         course = course->next;
         index++;
     }
