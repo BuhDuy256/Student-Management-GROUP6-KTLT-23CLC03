c905915bdfe1f70d7aa8a1f69acc822ac76bd83e
Huy Tran <huycamhuy@gmail.com>
2024-03-26 20:59:58 +0700

    Add function getChoice
    This function get numberOfChoices, the input prompt, choice result as parameters.
    The choice result is passed by reference.
    This function return boolean stands for the user input is valid or not.

---
+++ SOURCE/Course/System.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include"Node.h"
+#include"Staff.h"
+#include"Student.h"
+// #include"Class.h"
+#include"Course.h"
+// #include"Semester.h"
+// #include"SchoolYear.h"
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iterator>
+#include <list>
+#include <bitset>
+
+bool getChoice(int numberOfChoices, std::string prompt, int &choice);
