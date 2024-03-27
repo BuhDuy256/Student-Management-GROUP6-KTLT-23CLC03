561eafb6f2abb6409749db6c4047f9bc2acc9ebc
Buh Duy <148170993+BuhDuy256@users.noreply.github.com>
2024-03-27 21:16:06 +0700

    Test Code in Main.cpp

--- SOURCE/main.cpp
+++ SOURCE/main.cpp
@@ -1,5 +1,39 @@
-#include"System.h"
+#include"system.h"
+#include<iostream>
+#include<chrono>
+
+std::string myID;
+Semester currSem;
+Node<SchoolYear>* currYear;
+Node<Course>* currCourse;
 
 int main () {
+	// Start the timer
+	auto start = std::chrono::high_resolution_clock::now();
 	
+	importAllSchoolYearsCSV();
+	importAllClassesCSV();
+	importAllStudentsCSV();
+	importAllSemestersCSV();
+	importAllCoursesCSV();
+	importAllStudentsInAllCoursesCSV();
+
+	saveAllSchoolYearsData();
+	saveAllClassesData();
+	saveAllStudentsData();
+	saveAllSemestersData();
+	saveAllCoursesData();
+	saveAllScoreboardsData();
+
+	deleteAllStudentsData();
+	deleteAllClassesData();
+	deleteAllScoreboardsData();
+	deleteAllCourseData();
+
+	auto end = std::chrono::high_resolution_clock::now();
+	// Calculate the duration
+	std::chrono::duration<double> duration = end - start;
+	// Print the duration in seconds
+	std::cout << "\nExecution time: " << duration.count() << " seconds." << std::endl;
+	return 0;
 }
