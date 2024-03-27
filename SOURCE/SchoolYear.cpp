5bd6c6ecfaa9294d0abd2af939b5e5023b0f310e
Huy Tran <huycamhuy@gmail.com>
2024-03-27 17:29:41 +0700

    Implement function createANewSemester
    This function checks if there have been 3 semesters this school year. If not, it creates a new semester and updates the current semester variable.

--- SOURCE/SchoolYear.cpp
+++ SOURCE/SchoolYear.cpp
@@ -1,2 +1,16 @@
-#include"SchoolYear.h"
+#include "SchoolYear.h"
+#include "GlobalVariables.h"
+
+bool SchoolYear::createANewSemester()
+{
+    if (semesters[2].isCreated == true)
+        return false;
+
+    int i = 0;
+    while (semesters[i].isCreated == true) i++;
+    semesters[i].isCreated = true;
+    currSem = semesters[i];
+    return true;
+}
+
 
--- SOURCE/SchoolYear.h
+++ SOURCE/SchoolYear.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include"Semester.h"
-#include"Class.h"
-#include<iostream>
+#include "Semester.h"
+#include "Class.h"
+#include <iostream>
 
 struct SchoolYear {
 	std::string year;
 	Node<Class>* classes = nullptr;
 	Semester semesters[3];
+
+	bool createANewSemester();
 };
 
