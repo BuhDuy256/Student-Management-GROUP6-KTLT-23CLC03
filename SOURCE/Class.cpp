53bb649d0011d04ec2c4fb1c974557b20591b1d3
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2024-04-01 21:37:29 +0700

    fixx

--- SOURCE/Class.cpp
+++ SOURCE/Class.cpp
@@ -87,7 +87,7 @@ int countUniqueTempCourses(Node<std::string>* TempCourses);
 int countUniqueTempCourses2(Node<std::string>* TempCourses, bool* check);
 bool Exist(bool* check, Node<Course>* checkexist, Node<std::string>* TempCourse, int n, int& ind);
 Node<std::string>* ClassCourse(Node<std::string>* TempCourses, bool* check);
-Node<Class>* ChooseClass(std::string choice);
+Node<Class>* ChooseClass(int choice);
 void previous(double& previoussum, double& previousnumofacticour, Node<Student>* StuScore, Node<Class>* ChosenClass,Node<std::string>* StuUniqueCours);
 
 //class.h:
