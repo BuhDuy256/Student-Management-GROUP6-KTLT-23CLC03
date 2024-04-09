f3f70dcee13bf381f75982f80e19c23d25f2c354
Huy Tran <huycamhuy@gmail.com>
2024-04-09 19:10:04 +0700

    Add padding to change password

--- StudentManagementSystem/mainwindow.cpp
+++ StudentManagementSystem/mainwindow.cpp
@@ -101,6 +101,8 @@ MainWindow::MainWindow(QWidget* parent)
     ui->lb_AdGender->setFont(minecraftFont);
     ui->lb_AdBirthday->setFont(minecraftFont);
     ui->lb_AdSocialID->setFont(minecraftFont);
+    ui->lb_latestSem->setFont(minecraftFont);
+    ui->lb_latestSY->setFont(minecraftFont);
     minecraftFont.setBold(0); //End Bold Region
 
 
