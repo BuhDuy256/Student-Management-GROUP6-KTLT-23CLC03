91998c0ba25d325105037fd6adef44a0e4561f90
Buh Duy <148170993+BuhDuy256@users.noreply.github.com>
2024-04-30 13:48:32 +0700

    Delete All Files feature/changePassword+savePassword

--- SOURCE/GlobalVariables.cpp
+++
@@ -1,5 +0,0 @@
-#include "GlobalVariables.h"
-
-std::string myID = "";
-Student students[1000];
-int numStudents = 0;
--- SOURCE/GlobalVariables.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "System.h"
-
-extern std::string myID;
-extern Student students[1000];
-extern int numStudents;
