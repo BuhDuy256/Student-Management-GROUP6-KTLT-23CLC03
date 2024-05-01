c080490f1374814f37fa6b59fec6cb7cbb325666
Buh Duy <148170993+BuhDuy256@users.noreply.github.com>
2024-05-01 20:13:56 +0700

    .

--- SOURCE/GlobalVariables.cpp
+++
@@ -1,7 +0,0 @@
-#include "GlobalVariables.h"
-
-std::string myID = "";
-Semester currSem;
-Node<SchoolYear>* currYear;
-Node<Course>* currCourse;
-Node<Staff>* currStaff;
--- SOURCE/System.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include"Node.h"
-#include"Staff.h"
-#include"Student.h"
-#include"Class.h"
-#include"Course.h"
-#include"SchoolYear.h"
-#include"GlobalVariables.h"
